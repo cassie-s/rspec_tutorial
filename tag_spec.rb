abf6baceea27081adb6a920756477fee248739ed
Cassie Simpson <cassie.s.simpson@gmail.com>
2023-06-05 13:49:55 -0500

    first commit

---
+++ tag_spec.rb
@@ -0,0 +1,12 @@
+describe "How to run specific Examples with Tags" do 
+  it 'is a slow test', :slow => true do 
+     sleep 10 
+     puts 'This test is slow!' 
+  end 
+  
+  it 'is a fast test', :fast => true do 
+     puts 'This test is fast!' 
+  end 
+end
+
+# run rspec --tag slow tag_spec.rb to run slow test only
